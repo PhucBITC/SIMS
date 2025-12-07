d621c9def057b285a6443fa0219c3efff8875f66
PhucBITC <phucxo262@gmail.com>
2025-12-07 22:04:27 +0700

    update interface dashboard

--- SIMS_Project/Program.cs
+++ SIMS_Project/Program.cs
@@ -32,6 +32,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseStaticFiles();
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthentication(); // Bật tính năng xác thực
